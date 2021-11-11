d3a951544d8b4d870381031fcf16a1a3442d8de9
Kumol Yotech <kumol@yotech.ltd>
2021-11-11 17:58:27 +0600

    drf

--- pages/expense.js
+++ pages/expense.js
@@ -1,11 +1,22 @@
+import { DatePicker, LocalizationProvider } from "@mui/lab";
 import React, { useState } from "react";
 const classes = import("../styles/Expense.module.css");
-
+import DateAdapter from '@mui/lab/AdapterMoment';
+import { TextField } from "@material-ui/core";
 const Expense = () => {
-  const [startDate, setStartDate] = useState(new Date());
+  const [value, setValue] = useState(new Date());
   return (
     <>
-      <div>Hello </div> 
+      <LocalizationProvider dateAdapter={DateAdapter}>
+        <DatePicker
+          label="Basic example"
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
     </>
   );
 };
