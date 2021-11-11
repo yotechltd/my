f5dae9b927423238d4772e26ff1465696c865f80
Kumol Yotech <kumol@yotech.ltd>
2021-11-11 18:48:02 +0600

    DatePicker

--- pages/expense.js
+++ pages/expense.js
@@ -3,14 +3,21 @@ import React, { useState } from "react";
 const classes = import("../styles/Expense.module.css");
 import DateAdapter from '@mui/lab/AdapterMoment';
 import { TextField } from "@material-ui/core";
+import moment from "moment";
 const Expense = () => {
   const [value, setValue] = useState(new Date());
+  function disableDate(day) {
+    //console.log(moment().format('dddd'));
+    let dayName = moment(day).format('dddd');
+    return dayName == "Thursday" || dayName == "Sunday";
+  }
   return (
     <>
       <LocalizationProvider dateAdapter={DateAdapter}>
         <DatePicker
           label="Basic example"
           value={value}
+          shouldDisableDate={disableDate}
           onChange={(newValue) => {
             setValue(newValue);
           }}
